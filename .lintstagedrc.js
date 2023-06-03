17930b084a937902c4b0c9070002fcda8ce0bb60
Tuvshinbayar Tuvshinzul <giva9712@gmail.com>
2023-06-04 00:54:54 +0800

    feat: initialize onis-backend

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(js|jsx|ts|tsx)': () =>
-    'nx affected --target tsc --noEmit --uncommitted',
+  // '**/*.(js|jsx|ts|tsx)': () =>
+  //   'nx affected --target tsc --noEmit --uncommitted',
 
   // Run test
-  '**/*.spec.(ts|tsx)': () => 'nx affected:test --uncommitted',
+  // '**/*.spec.(ts|tsx)': () => 'nx affected:test --uncommitted',
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': () => [
-    `nx affected:lint --uncommitted --fix true`,
+    // `nx affected:lint --uncommitted --fix true`,
     `nx format:write --uncommitted`,
     `git add`
   ],
